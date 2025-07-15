3da2127cea6610426cff54e64ebbf798d562bc49
BombsRoyal <logange1223@gmail.com>
2025-07-15 17:33:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -60,6 +60,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
